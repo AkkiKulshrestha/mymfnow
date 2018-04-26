cbf716328ebb0087413cb61ef63cc8a6320d2871
Akshay <akkikulshrestha28@gmail.com>
2018-04-26 17:27:31 +0530

    Login Design

--- app/src/main/java/com/indocosmic/mymfnow/LoginScreen.java
+++ app/src/main/java/com/indocosmic/mymfnow/LoginScreen.java
@@ -1,6 +1,70 @@
 package com.indocosmic.mymfnow;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class LoginScreen extends AppCompatActivity {
+
+    LinearLayout LayoutTabUser,LayoutTabAdvisor,Layout_user_login,Layout_advisor_login;
+    EditText Edt_PanNo;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        init();
+
+    }
+
+    private void init() {
+
+        LayoutTabUser= (LinearLayout)findViewById(R.id.LayoutTabUser);
+
+        LayoutTabAdvisor= (LinearLayout)findViewById(R.id.LayoutTabAdvisor);
+
+        Layout_user_login= (LinearLayout)findViewById(R.id.Layout_user_login);
+        Layout_advisor_login= (LinearLayout)findViewById(R.id.Layout_advisor_login);
+
+        LayoutTabUser.setBackgroundColor(getResources().getColor(R.color.colorPrimaryLight));
+        LayoutTabAdvisor.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+
+        Layout_user_login.setVisibility(View.VISIBLE);
+        Layout_advisor_login.setVisibility(View.GONE);
+
+        LayoutTabUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutTabUser.setBackgroundColor(getResources().getColor(R.color.colorPrimaryLight));
+                LayoutTabAdvisor.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                Layout_user_login.setVisibility(View.VISIBLE);
+                Layout_advisor_login.setVisibility(View.GONE);
+
+
+            }
+        });
+
+        LayoutTabAdvisor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutTabUser.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                LayoutTabAdvisor.setBackgroundColor(getResources().getColor(R.color.colorPrimaryLight));
+                Layout_user_login.setVisibility(View.GONE);
+                Layout_advisor_login.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        Edt_PanNo = (EditText)findViewById(R.id.Edt_PanNo);
+        Edt_PanNo.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
+
+
+    }
+
 }
